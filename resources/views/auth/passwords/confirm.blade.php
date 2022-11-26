d5c84f408ab0ff4c33d3e0c25cde0306c68ccabf
Israel Jara <israel.sebastian92@gmail.com>
2022-11-26 19:04:05 -0300

    Ajuste de login layouts

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,42 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+    <div class="text-center">
+        <h1 class="h4 text-gray-900 mb-4">Confirme su contraseña antes de continuar</h1>
+    </div>
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+    <form class="user" method="POST" action="{{ route('password.confirm') }}">
+        @csrf
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
+        <div class="row mb-3">
+            {{-- <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label> --}}
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+            <div class="col-md-12">
+                <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror"
+                    name="password" required autocomplete="current-password" placeholder="Contraseña">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="row mb-0">
+            <div class="col-md-12 offset-md-12">
+                <button type="submit" class="btn btn-primary btn-user btn-block">
+                    {{ __('Confirmar Contraseña') }}
+                </button>
+
+                <hr>
+
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Recuperar Contraseña') }}
+                    </a>
+                @endif
+            </div>
+        </div>
+    </form>
 @endsection
